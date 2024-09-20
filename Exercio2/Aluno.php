af7e8befb823e7ceaecd2e69084aca19440e8143
wakeupt <143895478+wakeupt@users.noreply.github.com>
2024-09-20 18:44:50 -0300

    Delete Exercio2 directory

--- Exercio2/Aluno.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $Nome = "Carilinho";
-   $Disc = "Matematica";
-   $N1 = 1.5;
-   $N2 = 4.5; $Med = ($N1 + $N2) /2;
-   Echo "O caro aluno ".$Nome.", na Disciplina de ".$Disc." Fecham com a média ".$Med;
-?>
-
